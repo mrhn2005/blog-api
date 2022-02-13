24bb26587ca07f698c45c37d6e15ace2cd32aec4
Masoud reyhanian <mrhn2005@gmail.com>
2022-02-13 10:24:40 +0330

    post scafolding and migration

---
+++ database/migrations/2022_02_13_062845_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 256);
+            $table->text('content');
+            $table->string('image')->nullable();
+            $table->foreignId('user_id')
+                ->foreign('user_id')
+                ->references('id')
+                ->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
